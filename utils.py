6f9f37c574864e05c7fd62dc257e77ae85281471
suptoasty <jason.lonsinger@gmail.com>
2019-10-03 00:47:49 -0500

    modal started for password input...need to find best way to take password from modal

--- utils.py
+++ utils.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python3
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QMenuBar, QProgressBar, QFileDialog, QTextEdit, QVBoxLayout, QGridLayout, QMenu, QAction, QMessageBox
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QButtonGroup, QLabel, QMenuBar, QProgressBar, QFileDialog, QTextEdit, QVBoxLayout, QGridLayout, QMenu, QAction, QMessageBox, QLineEdit, QDialog, QDialogButtonBox, QInputDialog
+from PyQt5.QtGui import QIcon, QTextOption
+from PyQt5.Qt import QCryptographicHash as crypto
 import sys
 import types
 import io
+
+class password_dialog(QDialog):
+	layout: QVBoxLayout = None
+	password: QLineEdit = None
+	confirm: QPushButton = None
+	cancel: QPushButton = None
+
+	def __init__(self):
+		QMessageBox.__init__(self)
+
+		self.layout = QVBoxLayout()
+		self.password = QLineEdit()
+		self.confirm = QPushButton()
+		self.cancel = QPushButton()
+
+		self.password.setText("")
+		self.confirm.setText("Confirm")
+		self.cancel.setText("Cancel")
+
+		self.layout.addWidget(self.password)
+		self.layout.addWidget(self.confirm)
+		self.layout.addWidget(self.cancel)
+		self.setLayout(self.layout)
+
+		self.password.setEchoMode(QLineEdit.Password)
+
+		self.setModal(True)
+		
 
 class app(QApplication):
 	menu: QMenuBar = None
@@ -11,6 +40,9 @@ class app(QApplication):
 	layout: QGridLayout = None
 	text_edit: QTextEdit = None
 	file_chooser: QFileDialog = None
+	password_dialog: password_dialog = None
+
+	word_wrap: bool = False
 
 	def getWindow(self) -> QWidget:
 		return self.window
@@ -33,8 +65,8 @@ class app(QApplication):
 		file_name, _ = QFileDialog.getOpenFileName(self.window,"QFileDialog.getOpenFileName()", "","All Files (*);;Text Files (*.txt)", options=options)
 		if(file_name):
 			print(file_name)
-			file = open(file_name, 'r')
-			self.text_edit.setText(file.read())
+			file = open(file_name, 'rb')
+			self.text_edit.setText(file.read().decode("utf-8"))
 			file.close()
 
 	def saveFile(self):
@@ -44,8 +76,28 @@ class app(QApplication):
 		if(file_name):
 			print(file_name)
 			file = open(file_name, 'wb')
-			file.write(bin(self.text_edit.toPlainText()))
+			file.write(bytes(self.text_edit.toPlainText(), "utf-8"))
 			file.close()
+	
+	def saveFileEncrpted(self):
+		options = QFileDialog.Options()
+		options |= QFileDialog.DontUseNativeDialog
+		file_name, _ = QFileDialog.getSaveFileName(self.window,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+		if(file_name):
+			# self.password_dialog.show()
+			self.password_dialog.exec()
+			print(file_name)
+			file = open(file_name, 'wb')
+			file.write(bytes(self.text_edit.toPlainText(), "utf-8"))
+			file.close()
+	
+	def toggleWordWrap(self):
+		self.word_wrap != self.word_wrap
+		if(self.word_wrap):
+			# self.text_edit.setLineWrapMode(self, QTextOption.WrapAnywhere)
+			self.text_edit.setLineWrapColumnOrWidth(1)
+		else:
+			self.text_edit.setLineWrapColumnOrWidth(0)
 
 	def __init__(self, args, title=None):
 		QApplication.__init__(self, args)
@@ -55,10 +107,13 @@ class app(QApplication):
 		self.window = QWidget()
 		self.layout = QGridLayout()
 		self.menu = QMenuBar()
+		self.password_dialog = QLineEdit()
 		self.text_edit = QTextEdit()
 		self.file_chooser = QFileDialog()
-
+		
 		self.window.setWindowTitle(title)
+		self.password_dialog = password_dialog()
+
 		# add menu
 		self.layout.addWidget(self.menu, 0, 0)
 		actionFile = self.menu.addMenu("&File")
@@ -78,12 +133,24 @@ class app(QApplication):
 		saveAction.triggered.connect(self.saveFile)
 		actionFile.addAction(saveAction)
 
+		saveAction = QAction(QIcon(), "&Save Encrypted", self)
+		saveAction.setShortcut("Ctrl+Alt+S")
+		saveAction.triggered.connect(self.saveFileEncrpted)
+		actionFile.addAction(saveAction)
+
 		actionFile.addSeparator()
 
 		exitAction = QAction(QIcon(), "&Exit", self)
 		exitAction.setShortcut("Ctrl+Q")
 		exitAction.triggered.connect(self.quit)
 		actionFile.addAction(exitAction)
+
+		actionFormat = self.menu.addMenu("&Format")
+
+		wordWrapAction = QAction(QIcon(), "&WordWrap", self)
+		wordWrapAction.setShortcut("Ctrl+W")
+		wordWrapAction.triggered.connect(self.toggleWordWrap)
+		actionFormat.addAction(wordWrapAction)
 
 		self.layout.addWidget(self.text_edit, 1, 0)
 
